753ab53819409feb2dc770826f44213d50596057
wey1998 <yitwey1998@hotmail.com>
2022-11-25 18:32:21 +0800

    Update web_scrape.py

--- web-scrapping/web_scrape.py
+++ web-scrapping/web_scrape.py
@@ -6,4 +6,27 @@ html = BeautifulSoup(webpage.content, "html.parser")
 
 print(html.prettify())
 print(html.title)
+print(html.find("a", class_="sc-bfec09a1-1 gfeYgX").string)
+print(html.find("a", class_="sc-bfec09a1-2 sc-bfec09a1-3 gwnRuk lbPvhp title-cast-item__char").string)
 
+actor = html.find_all("a", class_="sc-bfec09a1-1 gfeYgX")
+profile = html.find_all(class_ = "sc-bfec09a1-1 gfeYgX",id="href")
+character = html.find_all(class_="sc-bfec09a1-4 kqwseC")
+
+print(html.find(class_ ="sc-bfec09a1-1 gfeYgX").get('href'))
+print(actor[1].get('href'))
+
+actor_name = []
+actor_page = []
+character_name = []
+
+for person, character in zip(actor,character):
+    print(person.prettify())
+    actor_name.append(person.string)
+    character_name.append(character.string)
+    actor_page.append(profile[person].get('href'))
+    
+    
+print(actor_page)
+print(actor_name)
+print(character_name)
