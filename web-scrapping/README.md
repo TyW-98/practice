9779704dbd9f526487a3dcb5406b82d0c10ac7b3
wey1998 <yitwey1998@hotmail.com>
2022-11-29 17:06:12 +0800

    selenium_praticals.py can scrape the whole page and updated README.md

--- web-scrapping/README.md
+++ web-scrapping/README.md
@@ -5,5 +5,8 @@ A few scrapers which can be use to extract the desired information from a websit
 The `Movie_scraper.py` allows users to information of a movie such as the actor/actress, their profile page and the other movies they are involved in from imdb website. 
 
 ## __Zoopla Property Scraper__ 
-The `zoopla_property_scraper.py` scrapers the desired property information from 
+The `zoopla_property_scraper.py` scrapes the desired property information from 
 [Zoopla's website](https://www.zoopla.co.uk/). It will scrape the property's name, value, location and the agent that is responsible for it. 
+
+## __Selenium Practicals__ 
+The `selenium_practicals.py` is similar to `zoopla_property_scraper.py`. The difference between the two scraper is that `selenium_practicals.py` uses Selenium and chromdriver to interact with the web browser to scrape information from the website.
--- web-scrapping/selenium_practials.py
+++ web-scrapping/selenium_practials.py
@@ -17,32 +17,49 @@ except:
     pass
 
 time.sleep(5)
-listing_property = driver.find_element(by= By.XPATH, value = '//*[@id="listing_63347676"]') # Get elements of a single listing
-a_tag = listing_property.find_element(by=By.TAG_NAME, value = "a") # Get "a" tag within listing property to get "href" attribute
-property_link = a_tag.get_attribute("href")
-print(property_link)
 
-driver.get(property_link)
-property_value = driver.find_element(by= By.XPATH, value = '//*[@class="c-PJLV c-PJLV-igTumRt-css"]')
-property_value = property_value.find_element(by = By.TAG_NAME, value = "p").text
-property_dict["Property_Value"].append(property_value)
-property_features = driver.find_elements(by = By.XPATH, value = '//*[@class= "c-PJLV c-PJLV-kQvhQW-centered-true c-PJLV-iPJLV-css"]')
-number_of_bedrooms = property_features[2].text
-property_dict["Bedrooms"].append(number_of_bedrooms)
-number_of_bathrooms = property_features[3].text
-property_dict["Bathrooms"].append(number_of_bathrooms)
-reception_yn = property_features[4].text
-if reception_yn == None:
-    reception_yn = "no"
-else:
-    reception_yn = "yes"
-property_dict["Reception"].append(reception_yn)
-property_area = property_features[5].text
-property_dict["Area"].append(property_area)
-property_address = driver.find_element(by = By.XPATH, value = '//*[@data-testid= "address-label"]').text
-property_dict["Property Address"].append(property_address)
-property_description = driver.find_element(by = By.XPATH, value = '//*[@data-testid = "listing_features_bulletted"]').text.replace("\n",", ")
-property_dict["Property Description"].append(property_description)
-print(property_dict)
-time.sleep(5)
+all_listing = driver.find_elements(by= By.XPATH, value = '//div[@class= "c-PJLV c-PJLV-igALLAE-css"]')
+print(all_listing[1].find_element(by = By.TAG_NAME, value = "a").get_attribute("href"))
 
+listing_link_list = []
+
+for listing in all_listing: 
+    listing_link = listing.find_element(by = By.TAG_NAME, value = "a").get_attribute("href")
+    listing_link_list.append(listing_link)
+
+for listing in listing_link_list:
+    driver.get(listing)
+    property_value = driver.find_element(by= By.XPATH, value = '//*[@class="c-PJLV c-PJLV-igTumRt-css"]')
+    property_value = property_value.find_element(by = By.TAG_NAME, value = "p").text
+    property_dict["Property_Value"].append(property_value)
+    property_features = driver.find_elements(by = By.XPATH, value = '//*[@class= "c-PJLV c-PJLV-kQvhQW-centered-true c-PJLV-iPJLV-css"]')
+    number_of_listed_features = len(property_features)
+    number_of_bedrooms = property_features[2].text
+    property_dict["Bedrooms"].append(number_of_bedrooms)
+    number_of_bathrooms = property_features[3].text
+    property_dict["Bathrooms"].append(number_of_bathrooms)
+    
+    if number_of_listed_features == 4:
+        reception_yn = "no"
+    else:
+        reception_yn = property_features[4].text
+        
+        if reception_yn == None:
+            reception_yn = "no"
+        
+        reception_yn = "yes"
+    
+    property_dict["Reception"].append(reception_yn)
+    
+    if number_of_listed_features == 6:
+        property_area = property_features[5].text    
+    else:
+        property_area = "Not Listed"
+        
+    property_dict["Area"].append(property_area)
+    property_address = driver.find_element(by = By.XPATH, value = '//*[@data-testid= "address-label"]').text
+    property_dict["Property Address"].append(property_address)
+    property_description = driver.find_element(by = By.XPATH, value = '//*[@data-testid = "listing_features_bulletted"]').text.replace("\n",", ")
+    property_dict["Property Description"].append(property_description)
+    print(property_dict)
+    time.sleep(5)
