53c0629f8a1fc73f6723f899cb0277c348a76171
TyW-98 <yitwey1998@hotmail.com>
2022-12-13 00:12:24 +0800

    Add functional-testing

---
+++ functional-testing/test_product.py
@@ -0,0 +1,25 @@
+from product import Product
+import unittest
+
+class productTestCase(unittest.TestCase):
+    
+    def test_transform_name(self):
+        small_black_shoes = Product("shoes","S","black")
+        expected_value = "SHOES"
+        actual_value = small_black_shoes.transform_name_for_sku()
+        self.assertEqual(expected_value,actual_value)
+        
+    def test_transform_color(self):
+        small_black_shoes = Product("Shoes","S","black")
+        expected_value = "BLACK"
+        actual_value = small_black_shoes.transform_color_for_sku()
+        self.assertEqual(expected_value,actual_value)
+        
+    def test_generate_sku(self):
+        small_black_shoes = Product("Shoes","S","black")
+        expected_value = "SHOES-S-BLACK"
+        actual_value = small_black_shoes.generate_sku()
+        self.assertEqual(expected_value,actual_value)
+        
+        
+unittest.main(argv =[""],verbosity= 0, exit= False)
