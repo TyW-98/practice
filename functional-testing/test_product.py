7feaa192589b3cbc3a7ad5b47be0803c071b1f66
TyW-98 <yitwey1998@hotmail.com>
2022-12-14 21:32:59 +0800

    Update test_product.py

--- functional-testing/test_product.py
+++ functional-testing/test_product.py
@@ -1,25 +1,33 @@
 from product import Product
 import unittest
 
-class productTestCase(unittest.TestCase):
+class ProductTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.handle = open("product.py","r")
+        self.product = Product("Shoes","S","black")
     
     def test_transform_name(self):
-        small_black_shoes = Product("shoes","S","black")
         expected_value = "SHOES"
-        actual_value = small_black_shoes.transform_name_for_sku()
+        actual_value = self.product.transform_name_for_sku()
         self.assertEqual(expected_value,actual_value)
         
     def test_transform_color(self):
-        small_black_shoes = Product("Shoes","S","black")
         expected_value = "BLACK"
-        actual_value = small_black_shoes.transform_color_for_sku()
+        actual_value = self.product.transform_color_for_sku()
         self.assertEqual(expected_value,actual_value)
         
     def test_generate_sku(self):
-        small_black_shoes = Product("Shoes","S","black")
         expected_value = "SHOES-S-BLACK"
-        actual_value = small_black_shoes.generate_sku()
+        actual_value = self.product.generate_sku()
         self.assertEqual(expected_value,actual_value)
         
+    def test_length(self):
+        file_length = len(self.handle.readlines())
+        self.assertTrue(file_length > 20)
         
-unittest.main(argv =[""],verbosity= 0, exit= False)
+    def tearDown(self):
+        del self.product
+        self.handle.close()
+        
+unittest.main(argv =[""],verbosity= 2, exit= False)
