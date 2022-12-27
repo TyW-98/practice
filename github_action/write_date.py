7c72381dd57bc4146181748f8398a90d70e4d834
wey1998 <yitwey1998@hotmail.com>
2022-12-27 18:06:47 +0800

    Edit write_date.py

--- github_action/write_date.py
+++ github_action/write_date.py
@@ -1,6 +1,4 @@
 from datetime import datetime
 
-if __name__ == "__main__":
-
-    with open("update_date.txt","w") as text_file:
-        text_file.write(f"The file is updated on {datetime.now()}")
+with open("github_action/update_date.txt","w") as text_file:
+    text_file.write(f'This repo is update on {datetime.now()}')
