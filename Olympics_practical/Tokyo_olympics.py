502c9f5b9a8aed14e041370794397e2845578541
wey1998 <yitwey1998@hotmail.com>
2022-12-30 13:52:59 +0800

    Add graphs to show data in dataframes

--- Olympics_practical/Tokyo_olympics.py
+++ Olympics_practical/Tokyo_olympics.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
-import copy
+import seaborn as sns
 import glob
+import matplotlib.pyplot as plt
 
 data_files = glob.glob(r"data"+"\*.xlsx")
-file_name_list = []
 dataframe_dict = {}
+column_names = {}
 
 for file in data_files:
-    print(file)
     file_name = file.replace(".xlsx","").split("\\")
     df = pd.read_excel(file)
     dataframe_dict[file_name[1]] = df
+    column_names[file_name[1]] = df.columns.values.tolist()
     
-print(list(dataframe_dict.keys()))
-
+Entries_gender = pd.melt(dataframe_dict["EntriesGender"].drop("Total", axis = 1),id_vars = "Discipline",var_name = "sex")
+print(Entries_gender)
+f = plt.figure(1)
+sns.barplot(data = Entries_gender, x = "Discipline", y = "value", hue = "sex")
+plt.show()
