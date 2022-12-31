f8aa36279c9f86b511690d22691a37a4378e905f
wey1998 <yitwey1998@hotmail.com>
2023-01-01 01:13:02 +0800

    Change x-axis tick angle

--- Olympics_practical/Tokyo_olympics.py
+++ Olympics_practical/Tokyo_olympics.py
@@ -19,12 +19,14 @@ print(column_names)
     
 Entries_gender = pd.melt(dataframe_dict["EntriesGender"].drop("Total", axis = 1),id_vars = "Discipline",var_name = "sex")
 fig_1 = px.bar(Entries_gender, x = "Discipline", y = "value", color = "sex", title = "Gender split in each sports")
+fig_1.update_layout(xaxis_tickangle = -45)
 fig_1.show()
 
 Athletes = dataframe_dict["Athletes"].drop("Name", axis = 1)
 Athletes = Athletes.groupby("NOC").size().reset_index()
 Athletes.rename(columns = {Athletes.columns[1]:"Number of Athletes"}, inplace = True)
 fig_2 = px.bar(Athletes, x = "NOC",y = "Number of Athletes", title = "Number of athletes per country")
+fig_2.update_layout(xaxis_tickangle = -45)
 fig_2.show()
 
 Teams = dataframe_dict["Teams"]
@@ -33,8 +35,10 @@ Teams.drop("Name", axis = 1, inplace = True)
 Teams = Teams.groupby("Discipline").size().reset_index()
 Teams.rename(columns = {Teams.columns[1]:"Number of Teams"},inplace = True)
 fig_3 = px.bar(Teams, x = "Discipline",y = "Number of Teams", title = "Number of teams in each discipline")
+fig_3.update_layout(xaxis_tickangle = -45)
 fig_3.show()
 
 Medals = dataframe_dict["Medals"]
 fig_4 = px.bar(Medals, x = "Team/NOC", y = ["Gold","Silver","Bronze"], title = "Each nation's medals")
+fig_4.update_layout(xaxis_tickangle = -45)
 fig_4.show()
