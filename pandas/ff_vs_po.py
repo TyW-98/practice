0b0ec94beb8ad280409fc2e6c87e53d0d3c4e668
wey1998 <yitwey1998@hotmail.com>
2023-01-06 23:46:11 +0800

    pandas practical update

--- pandas/ff_vs_po.py
+++ pandas/ff_vs_po.py
@@ -19,3 +19,16 @@ fire_mean_base_salary = round(fire_department["BasePay"].mean(),2)
 
 print(f"The number of police is {number_of_police} and their meant base pay is ${police_mean_base_salary}") 
 print(f"The number of police is {number_of_ff} and their meant base pay is ${fire_mean_base_salary}") 
+
+unique_jobs = list(df_salaries["JobTitle"].unique())
+number_of_unique_jobs = len(unique_jobs)
+
+print(f"There is {number_of_unique_jobs} jobs and it includes {unique_jobs}")
+
+first_name = df_salaries["EmployeeName"].apply(lambda x:x.split()[0])
+number_of_johns = first_name.apply(lambda x: x.count("JOHN")).sum()
+first_name_length = first_name[first_name.apply(lambda x: len(x) > 6)]
+
+print(number_of_johns)
+print(first_name_length)
+
